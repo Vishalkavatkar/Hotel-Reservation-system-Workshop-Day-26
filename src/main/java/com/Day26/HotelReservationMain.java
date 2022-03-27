5651ac90608eada1995d9b2e90a7c0d64fc14f9c
KAVATKAR <kavatkar-vishal.prabhakar@capgemini.com>
2022-03-27 11:58:31 +0530

    Special rates for reward customers

--- src/main/java/com/Day26/HotelReservationMain.java
+++ src/main/java/com/Day26/HotelReservationMain.java
@@ -4,6 +4,10 @@ import java.time.LocalDate;
 import java.time.Month;
 import java.util.Scanner;
 
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.Scanner;
+
 /**
  * This is a main class.
  * In this we are adding and displaying the hotel details.
@@ -12,16 +16,12 @@ public class HotelReservationMain {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Hotel Reservation System");
 		HotelReservation hotelReservation = new HotelReservation();
-		hotelReservation.addHotel("Lakewood", 3, 110, 90);
-		hotelReservation.addHotel("Bridgewood", 4, 150, 50);
-		hotelReservation.addHotel("Ridgewood", 5, 220, 150);
+		hotelReservation.addHotel("Lakewood", 3, 110, 90, 80, 80);
+		hotelReservation.addHotel("Bridgewood", 4, 150, 50, 110, 50);
+		hotelReservation.addHotel("Ridgewood", 5, 220, 150, 100, 40);;
 		
-		hotelReservation.displayHotel();
+		hotelReservation.printHotelList();
 		
-		
-		LocalDate startDate = LocalDate.of(2021, Month.SEPTEMBER, 11);    
-		LocalDate endDate = LocalDate.of(2021, Month.SEPTEMBER, 12);    
-		Hotel hotelName = hotelReservation.getCheapestBestRatedHotel(startDate, endDate);
-		System.out.println("The best rated hotel is : \n " + hotelName);
+	
 }
 }
